4844cd64a1a18e8dd27fa5b884087050d7ff3000
ggracez <gracezhu13@gmail.com>
2023-05-02 22:54:17 -0600

    more fun

--- testbed.py
+++ testbed.py
@@ -3,38 +3,47 @@ import matplotlib.pyplot as plt
 
 class TestBed():
     
-    def __init__(self, mean=0, stdev=1, arms=10) -> None:
+    def __init__(self, mean=0, stdev=1, arms=10, runs=2000) -> None:
         """10 different arms (options) with each option initialized from a normal/Gaussian distribution
 
         Args:
             mean (int, optional): _description_. Defaults to 0.
             stdev (int, optional): _description_. Defaults to 1.
             arms (int, optional): _description_. Defaults to 10.
+            runs (int, optional): how many bandit problems. Defaults to 2000.
         """
         self.mean = mean
         self.stdev = stdev
         self.arms = arms
+        self.runs = runs
         self.bandits = []
         
         # generate q*(a) for each action value
-        means = np.random.normal(self.mean, self.stdev, self.arms)
+        self.means = np.random.normal(self.mean, self.stdev, self.arms)
+        # print(means)
         
         for i in range(self.arms):
-            bandit = np.random.normal(means[i], self.stdev, 2000)  # 2000 iterations?
+            bandit = np.random.normal(self.means[i], self.stdev, self.runs)
             self.bandits.append(bandit)
     
+        # optimal action
+        self.opt = np.argmax(self.means)
+        # print(self.opt)
+        
     def show_plot(self) -> None:
+        """Generate a violin plot of the reward distributions for each action value, like Figure 2.1 in the textbook.
+        """
 
         # violin plot with means and stdevs
-        fig, ax = plt.subplots()  # idk what the point of subplots are tbh
-        ax.violinplot(dataset=self.bandits, showmeans=True)
+        fig, ax = plt.subplots()
+        ax.violinplot(dataset=self.bandits, showmeans=True, showextrema=False)
 
         # x-axis ticks for each action value
         arms = np.arange(1, self.arms+1)
         ax.set_xticks(arms)
 
         # # scatterplot of means only
-        # ax.scatter(arms, means)
+        # ax.scatter(arms, self.means)
         
         # set axis labels
         ax.set_ylabel("Reward Distribution")
@@ -46,7 +55,9 @@ class TestBed():
         ax.set_ylim(ymin=-y_max, ymax=y_max)
 
         plt.show()
-        # fig.savefig("violin.png")  # or plt.savefig ????
+        # fig.savefig("scatter.png")
+        # fig.savefig("violin.png")
+        # plt.close()
 
 testbed = TestBed()
 testbed.show_plot()
