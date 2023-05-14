649ab2753f6c288cba512a23b665e8c21902c6a9
ggracez <gracezhu13@gmail.com>
2023-05-14 15:47:24 -0600

    add plots for nonstationary

--- environment.py
+++ environment.py
@@ -4,16 +4,9 @@ import matplotlib.pyplot as plt
 
 class Environment:
 
-    def __init__(self, mean=0, stdev=1, arms=10, runs=2000, stationary=True, decay=1) -> None:
-        """Default 10 different arms (options) with each option initialized from a normal/Gaussian distribution
-
-        Args:
-            mean (int, optional): Mean for each arm. Defaults to 0.
-            stdev (int, optional): Standard deviation. Defaults to 1.
-            arms (int, optional): Number of arms. Defaults to 10.
-            runs (int, optional): Number of bandit problems. Defaults to 2000.
-            stationary (bool, optional): Whether the arms are stationary or not. Defaults to True.
-            decay (int, optional): Decay value for non-stationary arms. Defaults to 0.
+    def __init__(self, mean=0, stdev=1, arms=10, runs=2000, stationary=True, decay=1):
+        """Default 10 different arms (options) with each option initialized from a normal/Gaussian distribution.
+        Runs is the number of bandit problems.
         """
         self.mean = mean
         self.stdev = stdev
@@ -24,24 +17,44 @@ class Environment:
 
         self.reset()
 
-    def reset(self) -> None:
+    def reset(self):
         # generate q*(a) for each action value
         self.means = np.random.normal(self.mean, self.stdev, self.arms)
 
         if not self.stationary:
             # non-stationary environment: random walk arms
-            for i in range(self.arms):
-                walk_size = self.update_arms(self.decay)
-                self.means[i] += walk_size
+            self.update_arms()
 
         # optimal action
         self.opt = np.argmax(self.means)
 
-    def update_arms(self, decay) -> float:
-        walk_size = np.random.normal(self.mean, self.stdev)
-        return walk_size * decay
+    def update_arms(self):
+        """Random walk for non-stationary arms
+        """
+        if not self.stationary:
+            for i in range(self.arms):
+                walk_size = np.random.normal(self.mean, self.stdev)
+                self.means[i] += (walk_size * self.decay)  # higher decay means arms change more rapidly
 
-    def show_plot(self) -> None:
+    def plot_walk(self, trials):
+        """Plot changing means for non-stationary arms
+        Different arms have different means
+        Note that if the arms are stationary, the arms will be a horizontal line
+        """
+        true_rewards = np.zeros((trials, self.arms))
+        for trial in range(trials):
+            self.update_arms()
+            for i in range(self.arms):
+                true_rewards[trial, i] += self.means[i]
+        fig, ax = plt.subplots()
+        ax.plot(true_rewards)
+        ax.set_xlabel("Trial")
+        ax.set_ylabel("Mean")
+        ax.legend([f"Arm {i+1}" for i in range(self.arms)])
+        # fig.savefig("figures/nonstationary.png")
+        plt.show()
+
+    def show_plot(self):
         """Generate a violin plot of the reward distributions for each action value, like Figure 2.1 in the textbook.
         """
         bandits = []
@@ -76,5 +89,8 @@ class Environment:
         # plt.close()
 
 
-# testbed = TestBed()
-# testbed.show_plot()
+# # plot fig 2.1
+# environment = Environment()
+# environment.show_plot()
+# environment = Environment(arms=4, stationary=False, decay=0.05)
+# environment.plot_walk(300)
